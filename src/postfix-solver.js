f59832d3566daa3a96574bcf7954c0d45bed46c3
SC0d3r <sasan.kelishani@gmail.com>
2017-11-16 16:18:56 +0330

    add postfix-solver

---
+++ src/postfix-solver.js
@@ -0,0 +1,24 @@
+const Stack = require('./stack').Stack;
+const {err , isNum , isOp} = require('./uitls');
+const op = require('./op').op;
+
+function postfixSolver(postfixExpr = []){
+  const resultStack = new Stack(postfixExpr.length);
+  postfixExpr.forEach(calc);
+
+  return resultStack.size && resultStack.pop();
+
+  function calc(x,i){
+    
+    if(isNum(+x)) return resultStack.push(+x);
+    if(!isOp(x)) err('Weird Input by infixToPostfix => '+x);
+
+    resultStack.push(
+      op(x).eval(resultStack.pop(),resultStack.pop())
+    );
+  }
+} 
+
+module.exports = {
+  postfixSolver
+}
